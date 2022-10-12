4d6ec424e0b3f9586e0f1c29f47f533ac68aebdd
subhamkrai <srai@redhat.com>
2022-10-12 12:52:14 +0530

    core: log message when ceph status failed in peer
    print error message when `dr health` failed to get
    ceph status from peer cluster.
    
    Signed-off-by: subhamkrai <srai@redhat.com>

--- kubectl-rook-ceph.sh
+++ kubectl-rook-ceph.sh
@@ -554,6 +554,10 @@ function run_dr_health() {
   info_msg "running ceph status from peer cluster"
   set +e
   run_ceph_command -s --mon-host "$MON_HOST" --id rbd-mirror-peer --key "$PEER_KEY" "$@"
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    error_msg "failed to get ceph status from peer cluster, please check for network issues between the clusters"
+  fi
   # run rbd pool mirror status
   info_msg "running mirroring daemon health"
   run_rbd_command -p "$blockpool_name" mirror pool status
