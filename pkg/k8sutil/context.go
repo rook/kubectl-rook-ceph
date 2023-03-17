f2586425cb346d1d65a7551ed49b7dfca5d1fb13
Blaine Gardner <blaine.gardner@redhat.com>
2023-03-17 16:33:10 -0600

    remove controller-runtime client
    Because this is not an operator, we don't need to use the help
    controller-runtime provides. Additionally, all code was using the
    Kubernetes clientset, and a lesson-learned in Rook is that unit testing
    multiple types of clients gets to be a burden for complex code. Let's
    keep the number of client interfaces to a minimum.
    
    Signed-off-by: Blaine Gardner <blaine.gardner@redhat.com>

--- pkg/k8sutil/context.go
+++ pkg/k8sutil/context.go
@@ -19,7 +19,6 @@ package k8sutil
 import (
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 type Context struct {
@@ -28,7 +27,4 @@ type Context struct {
 
 	// Clientset is a connection to the core kubernetes API
 	Clientset kubernetes.Interface
-
-	// Represents the Client provided by the controller-runtime package to interact with Kubernetes objects
-	Client client.Client
 }
