a9410da8c28a844373b83110289ef1054e2b2871
shay1760 <rahulkrrai0302@gmail.com>
2024-02-19 14:59:40 +0530

    clean: remove krew installation
    removing krew installation as it was not used anywhere.
    Adding `clean` as commit title.
    
    Signed-off-by: shay1760 <rahulkrrai0302@gmail.com>

--- tests/github-action-helper.sh
+++ tests/github-action-helper.sh
@@ -8,19 +8,6 @@ set -xeEo pipefail
 : "${FUNCTION:=${1}}"
 : "${BLOCK:=$(sudo lsblk --paths | awk '/14G/ || /75G/ {print $1}' | head -1)}"
 
-# source https://krew.sigs.k8s.io/docs/user-guide/setup/install/
-install_krew() {
-  cd "$(mktemp -d)"
-  OS="$(uname | tr '[:upper:]' '[:lower:]')"
-  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
-  KREW="krew-${OS}_${ARCH}"
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz"
-  tar zxvf "${KREW}.tar.gz"
-  ./"${KREW}" install krew
-  cp "$HOME"/.krew/bin/kubectl-krew /usr/local/bin/
-  kubectl krew install rook-ceph
-}
-
 # source https://github.com/rook/rook
 use_local_disk() {
   BLOCK_DATA_PART=${BLOCK}1
